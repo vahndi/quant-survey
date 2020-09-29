612dcb258b00a46b2427d3c9cb218f5c745ff033
vahnd <vahndi.minah@gmail.com>
2020-09-29 08:24:55 -0400

    minor edits

--- survey/mixins/single_category_group/single_category_group_pt_mixin.py
+++ survey/mixins/single_category_group/single_category_group_pt_mixin.py
@@ -9,10 +9,7 @@ class SingleCategoryGroupPTMixin(object):
 
     name: str
     category_names: List[str]
-
-    @property
-    def items(self) -> list:
-        raise NotImplementedError
+    items: list
 
     @property
     def item_dict(self) -> dict:
@@ -21,23 +18,32 @@ class SingleCategoryGroupPTMixin(object):
     def cpt(self, item_name: str = 'item', value_name: str = 'value',
             item_names: Optional[List[str]] = None):
         """
-        Create a conditional probability table of the answer values for each question given the key to the question.
+        Create a conditional probability table of the answer values for each
+        question given the key to the question.
 
-        :param item_name: Name for the collection of questions or attributes to condition on.
-        :param value_name: Name for the collection of values to calculate conditional probability of.
-        :param item_names: Optional list of names for the questions or attributes instead of the keys.
+        :param item_name: Name for the collection of questions or attributes to
+                          condition on.
+        :param value_name: Name for the collection of values to calculate
+                           conditional probability of.
+        :param item_names: Optional list of names for the questions or
+                           attributes instead of the keys.
         """
         if item_names is not None:
             # stack with item names in index
-            number_mappings = {item_names.index(name): name for name in item_names}
-            item = self.stack(item_name, number_index=item_name, number_mappings=number_mappings)
+            number_mappings = {item_names.index(name):
+                                   name for name in item_names}
+            item = self.stack(item_name, number_index=item_name,
+                              number_mappings=number_mappings)
             cond_order = item_names
         else:
             # stack with keys in index
             item = self.stack(item_name, key_index=item_name)
-            cond_order = [k for q in self.items for k in self.item_dict.keys() if self.item_dict[k] == q]
-        instances = item.data.rename(value_name).reset_index()
-        index_name = [c for c in instances.columns if c not in (item_name, value_name)][0]
+            cond_order = [k for q in self.items
+                          for k in self.item_dict.keys()
+                          if self.item_dict[k] == q]
+        instances = item.sentiments.rename(value_name).reset_index()
+        index_name = [c for c in instances.columns
+                      if c not in (item_name, value_name)][0]
         instances = instances.set_index(index_name)
         cpt = create_cpt(
             prob_data=instances[value_name], cond_data=instances[item_name],
@@ -49,23 +55,32 @@ class SingleCategoryGroupPTMixin(object):
     def jpt(self, item_name: str = 'item', value_name: str = 'value',
             item_names: Optional[List[str]] = None):
         """
-        Create a joint probability table of the values for each item with the key to the item.
+        Create a joint probability table of the values for each item with the
+        key to the item.
 
-        :param item_name: Name for the collection of questions or attributes to calculate probability of.
-        :param value_name: Name for the collection of values to calculate probability of.
-        :param item_names: Optional list of names for the questions or attributes instead of the keys.
+        :param item_name: Name for the collection of questions or attributes to
+                          calculate probability of.
+        :param value_name: Name for the collection of values to calculate
+                           probability of.
+        :param item_names: Optional list of names for the questions or
+                           attributes instead of the keys.
         """
         if item_names is not None:
             # stack with item names in index
-            number_mappings = {item_names.index(name): name for name in item_names}
-            item = self.stack(item_name, number_index=item_name, number_mappings=number_mappings)
+            number_mappings = {item_names.index(name):
+                                   name for name in item_names}
+            item = self.stack(item_name, number_index=item_name,
+                              number_mappings=number_mappings)
             question_order = item_names
         else:
             # stack with keys in index
             item = self.stack(item_name, key_index=item_name)
-            question_order = [k for q in self.items for k in self.item_dict.keys() if self.item_dict[k] == q]
-        instances = item.data.rename(value_name).reset_index()
-        index_name = [c for c in instances.columns if c not in (item_name, value_name)][0]
+            question_order = [k for q in self.items
+                              for k in self.item_dict.keys()
+                              if self.item_dict[k] == q]
+        instances = item.sentiments.rename(value_name).reset_index()
+        index_name = [c for c in instances.columns
+                      if c not in (item_name, value_name)][0]
         instances = instances.set_index(index_name)
         jpt = create_jpt(
             data_1=instances[value_name], data_2=instances[item_name],
@@ -78,14 +93,19 @@ class SingleCategoryGroupPTMixin(object):
                  item_names: Optional[List[str]] = None,
                  **kwargs) -> Axes:
         """
-        Plot a conditional probability table of the answer values for each question given the key to the question.
+        Plot a conditional probability table of the answer values for each
+        question given the key to the question.
 
-        :param item_name: Name for the collection of questions or attributes to condition on.
-        :param value_name: Name for the collection of values to calculate conditional probability of.
-        :param item_names: Optional list of names for the questions or attributes instead of the keys.
+        :param item_name: Name for the collection of questions or attributes to
+                          condition on.
+        :param value_name: Name for the collection of values to calculate
+                           conditional probability of.
+        :param item_names: Optional list of names for the questions or
+                           attributes instead of the keys.
         :param kwargs: See utils.plots.plot_pt
         """
-        cpt = self.cpt(item_name=item_name, value_name=value_name, item_names=item_names)
+        cpt = self.cpt(item_name=item_name, value_name=value_name,
+                       item_names=item_names)
         if 'transpose' not in kwargs.keys():
             kwargs['transpose'] = True
         if 'var_sep' not in kwargs:
@@ -97,14 +117,19 @@ class SingleCategoryGroupPTMixin(object):
                  item_names: Optional[List[str]] = None,
                  **kwargs) -> Axes:
         """
-        Plot a joint probability table of the answer values for each question with the key to the question.
+        Plot a joint probability table of the answer values for each question
+        with the key to the question.
 
-        :param item_name: Name for the collection of questions or attributes to condition on.
-        :param value_name: Name for the collection of values to calculate conditional probability of.
-        :param item_names: Optional list of names for the questions or attributes instead of the keys.
+        :param item_name: Name for the collection of questions or attributes to
+        condition on.
+        :param value_name: Name for the collection of values to calculate
+                           conditional probability of.
+        :param item_names: Optional list of names for the questions or
+                           attributes instead of the keys.
         :param kwargs: See utils.plots.plot_pt
         """
-        jpt = self.jpt(item_name=item_name, value_name=value_name, item_names=item_names)
+        jpt = self.jpt(item_name=item_name, value_name=value_name,
+                       item_names=item_names)
         if 'transpose' not in kwargs.keys():
             kwargs['transpose'] = True
         if 'var_sep' not in kwargs:
