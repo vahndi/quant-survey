268e9aabaa9c853353442ea34206b09df522812d
vahndi.minah <vahndi.minah@frogdesign.com>
2021-08-29 13:51:58 -0400

    v0.0.003

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
     name='quant-survey',
     packages=find_packages(),
-    version='0.0.002',
+    version='0.0.003',
     license='MIT',
     description='Library for quantitative survey analysis.',
     author='Vahndi Minah',
